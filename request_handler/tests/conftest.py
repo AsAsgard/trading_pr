a97d2644c991d3515d246adca77a7d1572bc2c1e
AsAsgard <dbrylkin96@gmail.com>
2019-05-05 22:12:10 +0300

    Modified tests added

--- request_handler/tests/conftest.py
+++ request_handler/tests/conftest.py
@@ -2,19 +2,28 @@
 # coding: utf-8
 
 import pytest
+from sqlalchemy_utils import create_database, database_exists, drop_database
 from app import create_app
 from app.database import db
-from appconfig import currentConfig
+from appconfig import setConfig, getConfig, TestConfig
 
 
 @pytest.yield_fixture(scope="session")
 def app():
+    setConfig(TestConfig)
     app = create_app()
-    app.debug = True
-    app.config['TESTING'] = True
     yield app
 
 
 @pytest.yield_fixture(scope="session")
 def _db(app):
-    return db
+    dsn = getConfig().SQLALCHEMY_DATABASE_URI
+    assert not database_exists(dsn)
+    create_database(dsn)
+    assert database_exists(dsn)
+    with app.test_request_context():
+        db.create_all()
+    yield db
+    db.session.remove()
+    drop_database(dsn)
+    assert not database_exists(dsn)
