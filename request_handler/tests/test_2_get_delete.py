ba2500f453a71420c662bc03a5f29456cf1bd698
AsAsgard <dbrylkin96@gmail.com>
2019-05-07 07:31:37 +0300

    Added stubs

--- request_handler/tests/test_2_get_delete.py
+++ request_handler/tests/test_2_get_delete.py
@@ -6,97 +6,10 @@ from flask import url_for
 from app.db_entities.files_view import Files
 from app.db_entities.data_view import Data
 from app.schemas.data_schema import DataSchema
-from sqlalchemy.exc import OperationalError
-
-testFilenames = ['some_file', 'one_more_file', 'third_file']
-
-testData = [
-    {
-        'ticker': "TEST_TICKER",
-        'per': 1,
-        'date': "2019-05-08",
-        'time': "10:08:15",
-        'open': 174.15,
-        'close': 174.65,
-        'high': 175.15,
-        'low': 172.15,
-        'vol': 43223
-    },
-    {
-        'ticker': "TEST_TICKER",
-        'per': 1,
-        'date': "2019-05-08",
-        'time': "10:08:16",
-        'open': 173.15,
-        'close': 173.65,
-        'high': 174.15,
-        'low': 171.15,
-        'vol': 122423
-    },
-    {
-        'ticker': "TEST_TICKER",
-        'per': 1,
-        'date': "2019-05-08",
-        'time': "10:08:17",
-        'open': 174.85,
-        'close': 175.65,
-        'high': 176.15,
-        'low': 173.15,
-        'vol': 32223
-    },
-]
+from tests.t_data import testFilenames, testData
 
 
-class TestServer:
-    def test_creation(self, db_session):
-        try:
-            assert not db_session.query(Files).all()
-            assert not db_session.query(Data).all()
-        except OperationalError:
-            assert False
-        else:
-            assert True
-
-    @pytest.mark.parametrize('file, values_list', [pytest.param(Files(filename=fn), testData) for fn in testFilenames])
-    def test_transactional_add_delete(self, db_session, file, values_list):
-        db_session.add(file)
-        db_session.commit()
-        assert db_session.query(Files).all() == [file]
-        samples = []
-        data_schema = DataSchema()
-        for values in values_list:
-            values['fileid'] = file.fileid
-            sample = data_schema.load(values, session=db_session, partial=True).data
-            assert isinstance(sample, Data)
-            db_session.add(sample)
-            samples.append(sample)
-        db_session.commit()
-        assert db_session.query(Data).all() == samples
-        db_session.query(Data).delete()
-        db_session.query(Files).delete()
-        db_session.commit()
-        assert not db_session.query(Files).all()
-        assert not db_session.query(Data).all()
-
-    @pytest.mark.parametrize('file, values_list', [pytest.param(Files(filename=fn), testData) for fn in testFilenames])
-    def test_transactional_add(self, db_session, file, values_list):
-        db_session.add(file)
-        db_session.commit()
-        assert db_session.query(Files).all() == [file]
-        samples = []
-        data_schema = DataSchema()
-        for values in values_list:
-            values['fileid'] = file.fileid
-            sample = data_schema.load(values, session=db_session, partial=True).data
-            assert isinstance(sample, Data)
-            samples.append(sample)
-            db_session.add(sample)
-        db_session.commit()
-        assert db_session.query(Data).all() == samples
-
-    def test_nothing_changed(self, db_session):
-        assert not db_session.query(Files).all()
-        assert not db_session.query(Data).all()
+class TestGetDelete:
 
     @pytest.mark.parametrize('file, values_list', [pytest.param(Files(filename=fn), testData) for fn in testFilenames])
     def test_get(self, client, db_session, file, values_list):
---
+++ request_handler/tests/test_3_post.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import pytest
+
+
+class TestPost:
+    pass
